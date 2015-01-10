533a8896d99ee7787aefedd3fa1fd656de1ae339
ajithnn <ajithnn20@gmail.com>
2015-01-10 19:27:41 +0530

    MultiPart Post added using multer.

--- index.js
+++ index.js
@@ -5,11 +5,25 @@ var express = require('express')
 var http = require('http')
 var https = require('https')
 var path = require('path')
+var multer = require('multer')
+var bodyParser = require('body-parser')
 var app = express()
-
-
 var rport;
 var rports;
+//************************************************************************************************
+/** Utility function to get the right type of body parser for handling POST requests.**/
+function getContentType(options) {
+    if (options.contentType == "urlencoded") {
+        return bodyParser.urlencoded(options.config)
+    } else if (options.contentType == "json") {
+        return bodyParser.json(options.config)
+    } else if (options.contentType == "raw") {
+        return bodyParser.raw(options.config)
+    } else if (options.contentType == "text") {
+        return bodyParser.text(options.config)
+    }
+}
+//************************************************************************************************
 
 module.exports = {
     /** 
@@ -20,13 +34,13 @@ module.exports = {
             try {
                 var contentType = req.get('content-type') || "None";
                 if (typeof returnValueOrFunc == 'function') {
-                    res.send(returnValueOrFunc(false,req.query, contentType));
+                    res.send(returnValueOrFunc(false, req.query, contentType));
                 } else {
                     res.send(returnValueOrFunc)
                 }
             } catch (e) {
                 if (typeof returnValueOrFunc == 'function') {
-                    returnValueOrFunc(e,"","");
+                    returnValueOrFunc(e, "", "");
                 } else {
                     throw e;
                 }
@@ -41,13 +55,13 @@ module.exports = {
         app.get(onPath, function (req, res) {
             try {
                 if (typeof returnFileFunc == 'function') {
-                    res.sendFile(path.join(__dirname, '../../', returnFileFunc(false,req.query)));
+                    res.sendFile(path.join(__dirname, '../../', returnFileFunc(false, req.query)));
                 } else {
                     res.sendFile(path.join(__dirname, '../../', returnFileFunc))
                 }
             } catch (e) {
                 if (typeof returnFileFunc == 'function') {
-                    returnFileFunc(e,"");
+                    returnFileFunc(e, "");
                 } else {
                     throw e;
                 }
@@ -55,8 +69,49 @@ module.exports = {
         });
 
     },
-    post: function (onPath, data, returnFunction) {
-        return returnFunction();
+    /** POST function which takes the route and a return value OR Callback function. There is not version of POST which can return a file, as the             client needs to handle it by making a getfile call.**/
+    post: function (onPath, returnFunction, options) {
+        app.post(onPath, getContentType(options), function (req, res) {
+            try {
+                if (typeof returnFunction == 'function') {
+                    res.status(200).send(returnFunction(false, req.body));
+                } else {
+                    res.status(200).send(returnFunction);
+                }
+            } catch (e) {
+                if (typeof returnFunction == 'function') {
+                    res.status(400).send(returnFunction(e, ""));
+                } else {
+                    throw e;
+                }
+            }
+        });
+    },
+    /** MultiPart POST function with two options for either reading fields or files.**/
+    multipost: function (onPath, returnFunction, options) {
+        app.use(onPath, multer({
+            dest: options.FilePath,
+            rename:options.Rename
+        }));
+        app.post(onPath, function (req, res) {
+            try {
+                if (typeof returnFunction == 'function') {
+                    if (options.PostType == 'field') {
+                        res.status(200).send(returnFunction(false, req.body));
+                    } else if (options.PostType == 'file') {
+                        res.status(200).send(returnFunction(false, req.files));
+                    }
+                } else {
+                    res.status(200).send(returnFunction);
+                }
+            } catch (e) {
+                if (typeof returnFunction == 'function') {
+                    res.status(400).send(returnFunction(e, ""));
+                } else {
+                    throw e;
+                }
+            }
+        });
     },
     /** 
     listen function for http
@@ -81,3 +136,5 @@ module.exports = {
     port: rport,
     ports: rports
 };
+
+//************************************************************************************************
